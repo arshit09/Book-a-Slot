95c219e74f07d002c8ac675af8c23015f817b555
arshit09 <arshitvaghasiya29@gmail.com>
2024-12-23 03:03:07 -0500

    update code

--- check_visa.py
+++ check_visa.py
@@ -1,3 +1,7 @@
+# Display resolution: 1920 x 1080
+# Scale: 100%
+# Browser: Chrome (bookmark bar is hidden)
+
 import numpy as np
 import webbrowser
 import pyautogui
@@ -40,41 +44,50 @@ webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(
 
 webbrowser.get('chrome').open(
     "https://ais.usvisa-info.com/en-ca/niv/users/sign_in")
-time.sleep(2)
+time.sleep(5)
 pyautogui.press('pagedown')
 # click on "I have read..."
-pyautogui.click(411, 919, duration = 0.9)
+pyautogui.click(404, 684, duration = 0.9)
 # click on Sign in button
-pyautogui.click(448, 985)
+pyautogui.click(441, 766, duration = 0.5)
 # wait to load the page
-time.sleep(1.25)
+time.sleep(6)
 # click on Continue button
-pyautogui.click(1461, 317)
+pyautogui.click(1461, 346, duration = 0.5)
+# wait
+time.sleep(1)
 # click on "Reschedule Appointment"
-pyautogui.click(392, 544, duration = 0.9)
+pyautogui.click(624, 535, duration = 0.9)
 # click on "Reschedule Appointment" button
-pyautogui.click(673, 663, duration = 0.5)
+pyautogui.click(678, 674, duration = 0.5)
+# wait
+time.sleep(1)
+time.sleep(5)
 # click on center of "Date of Appointment" input box
-pyautogui.click(700, 664, duration = 0.5)
+pyautogui.click(691, 686, duration = 0.5)
 
 # wait to load calendar
-time.sleep(1.75)
-month_color = pyautogui.locateOnScreen(
-    'Images/month_back_button.png', confidence=0.9)
+time.sleep(2)
+try:
+    month_color = pyautogui.locateOnScreen(
+        'Images/month_back_button.png', confidence=0.9)
 
-if month_color == None:
+    if month_color is None:
+        print("Calendar not detected")
+except pyautogui.ImageNotFoundException:
     print("Calendar not detected")
 
 month_back_button_count = 0
 
-while (month_color != None) and (month_back_button_count != 6):
+while (month_color != None) and (month_back_button_count != 50):
     # calendar screenshot resolution
-    # 504 751
-    #         1000 911
-    image = pyautogui.screenshot(region=(504, 751, 496, 160))
+    # 488 775
+    #         1020 949
+    image = pyautogui.screenshot(region=(488, 775, 532, 174))
     image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-    cv2.imwrite("C://Users//arshit//Desktop//visa script//Images//Appointment.png", image)
-    color_thief = ColorThief("C://Users//arshit//Desktop//visa script//Images//Appointment.png")
+    cv2.imwrite(".//Images//Appointment.png", image)
+    time.sleep(1)
+    color_thief = ColorThief(".//Images//Appointment.png")
 
     # get the dominant color
     dominant_color = color_thief.get_color(quality=1)
@@ -85,7 +98,7 @@ while (month_color != None) and (month_back_button_count != 6):
     Py = palette[1][1]
     Pz = palette[1][2]
     if (Px >= 60 and Px <= 70) and (Py >= 120 and Py <= 130) and (Pz >= 162 and Pz <= 172):
-        x, y = 525, 775
+        x, y = 508, 798
         flag1 = 0
         for j in range(0, 5):
             for i in range(0, 7):
@@ -93,13 +106,33 @@ while (month_color != None) and (month_back_button_count != 6):
                 x += 35
             if flag1 != 4:
                 y += 30
-                x = 525
+                x = 508
                 flag1 += 1
 
         # check left calendar
-        month_color = pyautogui.locateOnScreen(
-            'Images/month_back_button.png', confidence=0.9)
-        if month_color == None:
+        try:
+            month_color = pyautogui.locateOnScreen(
+                './Images/month_back_button.png', confidence=0.9)
+            if month_color is None:
+                pyautogui.click(712, 751)
+                # down time slot
+                pyautogui.click(700, 798, duration=0.5)
+                # up time slot
+                pyautogui.click(704, 716, duration=0.5)
+                
+                image = pyautogui.screenshot(region=(496, 625, 168, 149))
+                image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+                cv2.imwrite("C://Users//arshit//Desktop//visa script v2//Images//Final_Appointment.png", image)
+
+                # send message in telegram group 
+                telegram_send_cmd = 'telegram-send --config group1.conf --image "C://Users//arshit//Desktop//visa script v2//Images//Final_Appointment.png" --caption "Slot opened at"'
+                os.system(telegram_send_cmd)
+
+                # click on green "Reschedule" button
+                pyautogui.click(1675, 873, duration=0.5)
+                break
+        except pyautogui.ImageNotFoundException:
+
             pyautogui.click(712, 751)
             # down time slot
             pyautogui.click(700, 798, duration=0.5)
@@ -118,7 +151,8 @@ while (month_color != None) and (month_back_button_count != 6):
             pyautogui.click(1675, 873, duration=0.5)
             break
 
-        x, y = 775, 773
+        # check right calendar
+        x, y = 777, 797
         flag2 = 0
         for j in range(0, 5):
             for i in range(0, 7):
@@ -126,21 +160,25 @@ while (month_color != None) and (month_back_button_count != 6):
                 x += 35
             if flag2 != 4:
                 y += 30
-                x = 775
+                x = 777
                 flag2 += 1
 
-        # check right calendar
-        month_color = pyautogui.locateOnScreen(
-            'Images/month_back_button.png', confidence=0.9)
-        if month_color == None:
+        try:
+            month_color = pyautogui.locateOnScreen(
+                './Images/month_back_button.png', confidence=0.9)
+            if month_color is None:
+                break
+        except pyautogui.ImageNotFoundException:
             break
     
     else:
         time.sleep(0.5)
-        pyautogui.click(986, 709)
+        # click on Next month button in the calendar
+        pyautogui.click(1003, 725)
     month_back_button_count += 1 
 
-pyautogui.hotkey('ctrl', 'w')
+# pyautogui.hotkey('ctrl', 'w')
+
 # config bot
 # telegram-send --config
 
